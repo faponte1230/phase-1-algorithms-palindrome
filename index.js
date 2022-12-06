d736aa09b1a7e3d7fec5d0ae5648d9958fb7b83f
Fernando Aponte <faponte1230@yahoo.com>
2022-12-05 20:00:28 -0500

    commit tree dbc6910b1cdaf8c0cdc23145ad185572444cae5e

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word){
+  return word.split('').reverse().join('')
 }
 
+function isPalindrome(word) {
+  const reversedWord = reverseString(word)
+  if (word === reversedWord){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+
 /* 
-  Add your pseudocode here
+  reverse the string
+  if the reversed string is the same as the input
+    return true
+  else
+    return false 
+    
+    Add your pseudocode here
 */
 
 /*
